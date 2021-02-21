84b7d70809bc330efe824f5a76745e44d58322ec
Solo4 <akasha02@mail.ru>
2021-02-21 23:44:41 +0300

    set map dates

--- app/src/main/java/by/roadstatistics/ui/daysPart/pickedDay/PicketDayFragment.kt
+++ app/src/main/java/by/roadstatistics/ui/daysPart/pickedDay/PicketDayFragment.kt
@@ -1,5 +1,6 @@
 package by.roadstatistics.ui.daysPart.pickedDay
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -7,13 +8,43 @@ import androidx.fragment.app.Fragment
 import by.roadstatistics.R
 import by.roadstatistics.database.CordInfo
 import by.roadstatistics.utils.Constants.BUNDLE_KEY_PICKET_DAY_FRAGMENT
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
 
-class PicketDayFragment : Fragment(R.layout.fragment_picket_day) {
+class PicketDayFragment : Fragment(R.layout.fragment_picket_day), OnMapReadyCallback {
+
+    private lateinit var localMap: GoogleMap
+    private lateinit var dayInfoList: ArrayList<CordInfo>
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val dayInfoList = arguments?.getParcelableArrayList<CordInfo>(BUNDLE_KEY_PICKET_DAY_FRAGMENT)
+        dayInfoList = arguments?.getParcelableArrayList<CordInfo>(BUNDLE_KEY_PICKET_DAY_FRAGMENT)
+            ?: arrayListOf()
+
+        val mapFragment = childFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        localMap = googleMap
+
+
+        //localMap.addMarker(MarkerOptions().title("Hello world").snippet("Additional text").position(LatLng(dayInfoList[0].longitude.toDouble(), dayInfoList[0].longitude.toDouble())))
+
+
+        PolylineOptions().also { polylineOptions ->
+            for (marker in dayInfoList) {
+                polylineOptions.add(LatLng(marker.latitude.toDouble(), marker.longitude.toDouble()))
+            }
+            localMap.addPolyline(polylineOptions)
+        }
 
 
 
